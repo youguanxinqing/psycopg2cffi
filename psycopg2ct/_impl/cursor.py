7d2457af828ce7ba8df48c39b92695a83165b5e9
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2012-10-15 11:58:29 +0400

    increase default itersize

--- psycopg2ct/_impl/cursor.py
+++ psycopg2ct/_impl/cursor.py
@@ -80,7 +80,7 @@ class Cursor(object):
         #: Read/write attribute specifying the number of rows to fetch from
         #: the backend at each network roundtrip during iteration on a named
         #: cursor. The default is 2000
-        self.itersize = 2000
+        self.itersize = 100000
 
         self.tzinfo_factory = tz.FixedOffsetTimezone
         self.row_factory = row_factory
