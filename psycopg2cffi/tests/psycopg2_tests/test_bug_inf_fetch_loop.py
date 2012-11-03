4ddbd593ca1ce8371701be399cd4f105d693a0a8
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2012-11-03 23:53:52 +0400

    trying to write a test for inf recursion bug

---
+++ psycopg2cffi/tests/psycopg2_tests/test_bug_inf_fetch_loop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# bug_info_fetch_loop.py - test for bug with infinite loop, when 
+# result size exceeds cursor.itersize
+
+import psycopg2
+from testconfig import dsn
+from testutils import unittest
+
+
+class CursorTests(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = psycopg2.connect(dsn)
+
+    def tearDown(self):
+        self.conn.close()
+
+    def test(self):
+        curs = self.conn.cursor()
+        curs.itersize = 10
+        curs.execute('create table inf_fetch_loop (id integer)')
+
+        for i in xrange(curs.itersize * 2):
+            curs.execute('insert into inf_fetch_loop values (%s)', (2 * i,))
+
+        curs.execute('select * from inf_fetch_loop')
+        result = curs.fetchall()
+        self.assertEqual(
+                result, 
+                [(2 * i,) for i in xrange(curs.itersize * 2)])
+
+
